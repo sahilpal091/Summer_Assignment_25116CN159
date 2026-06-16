e5e7d7befba511a50593d4108d76e2caadea9eaa
sahilpal091 <sahilpal091@gmail.com>
2026-06-16 20:23:01 +0530

    Add files via upload

---
+++ Day 5/Question17.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+
+sum = 0
+
+for i in range(1, num):
+    if num % i == 0:
+        sum = sum + i
+
+if sum == num:
+    print("Perfect Number")
+else:
+    print("Not a Perfect Number")
